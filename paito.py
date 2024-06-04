a4d7d51a1b0b4fb75b8ccb86019f959931ab07ae
isanb22 <111150909+isanb22@users.noreply.github.com>
2024-06-04 15:22:33 -0300

    to testando mais um pouco

--- paito.py
+++ paito.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
+#oi 
 class Grafo:
 
   def __init__(self, repr=None, direcionado=False, ponderado=False, arquivoPajek=None):
